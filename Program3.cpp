c4af9124ea6c8c732b685ad614bd0194f098395d
Sonu Kumar <110619659+sonuku092@users.noreply.github.com>
2023-06-25 22:03:57 +0530

    Done

--- Program3.cpp
+++ Program3.cpp
@@ -1,148 +1,175 @@
-#include<iostream> 
-#include<fstream> 
-#include<string.h> 
-#define SIZE 55 
-using namespace std; 
-char buffer[SIZE + 1]; 
- 
-class Student { 
-char usn[15]; 
-char name[20]; 
-char sem[5]; 
-char marks[10]; 
-public: 
-void getData(); 
-void putData(); 
-void pack(); 
-void unpack(); 
-void insert(); 
-void display(); 
-void modify(char *key); 
-void search(char *key); 
-}; 
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#define SIZE 55
+using namespace std;
+char buffer[SIZE + 1];
 
-void Student::getData() { 
-cout << "Enter usn, name, sem, marks: \n"; 
-cin >> usn >> name >> sem >> marks; 
-} 
- 
-void Student::putData() { 
-cout << usn << "\t" << name << "\t\t" << sem << "\t" << marks << endl; 
-} 
- 
-void Student::pack() { 
-strcpy(buffer, usn); strcat(buffer, "|"); 
-strcat(buffer, name); strcat(buffer, "|"); 
-strcat(buffer, sem); strcat(buffer, "|"); 
-strcat(buffer, marks); 
-strcat(buffer, "\n"); 
-} 
- 
-void Student::unpack() { 
-char *p; 
-p = strtok(buffer, "|"); strcpy(usn, p); 
-p = strtok(NULL, "|"); strcpy(name, p); 
-p = strtok(NULL, "|"); strcpy(sem, p); 
-p = strtok(NULL, "\n"); strcpy(marks, p);
+class Student
+{
+    char usn[15];
+    char name[20];
+    char sem[5];
+    char marks[10];
+
+public:
+    void getData();
+    void putData();
+    void pack();
+    void unpack();
+    void insert();
+    void display();
+    void modify(char *key);
+    void search(char *key);
+};
+
+void Student::getData()
+{
+    cout << "Enter usn, name, sem, marks: \n";
+    cin >> usn >> name >> sem >> marks;
 }
 
-void Student::insert() { 
-getData(); 
-pack(); //packs the data into buffer 
-ofstream fout("record.txt", ios::app); 
-fout << buffer; 
-fout.close(); 
-} 
- 
-void Student::display() { 
-ifstream fin("record.txt"); 
-while(!fin.eof()) { 
-fin.getline(buffer, SIZE + 1, '\n'); 
-if(fin.fail()) break; 
-unpack(); 
-putData(); 
-} 
-fin.close(); 
-} 
- 
- 
-void Student::search(char *key) { 
-ifstream fin("record.txt"); 
-int count = 0; 
-while(!fin.eof()) { 
-fin.getline(buffer, SIZE + 1, '\n');
-if(fin.fail()) break; 
-unpack(); 
-if(strcmp(usn, key) == 0) { 
-putData(); 
-count++; 
-} 
-} 
-cout << "Total records found: " << count << endl; 
-fin.close(); 
+void Student::putData()
+{
+    cout << usn << "\t" << name << "\t\t" << sem << "\t" << marks << endl;
 }
 
-void Student::modify(char *key) { 
-ifstream fin("record.txt"); 
-ofstream fout("temp.txt"); 
-int count = 0; 
-while(!fin.eof()) { 
-fin.getline(buffer, SIZE + 1, '\n'); 
-if(fin.fail()) break; 
-unpack(); 
-if(strcmp(usn, key) == 0) { 
-getData(); 
-count++; 
-} 
-pack(); 
-fout << buffer; 
-} 
-if(count == 0) 
-cout << "USN not found." << endl; 
-else 
-cout << "Modified. " << endl; 
- 
- 
-fin.close(); 
-fout.close(); 
-remove("record.txt"); 
-rename("temp.txt", "record.txt"); 
+void Student::pack()
+{
+    strcpy(buffer, usn);
+    strcat(buffer, "|");
+    strcat(buffer, name);
+    strcat(buffer, "|");
+    strcat(buffer, sem);
+    strcat(buffer, "|");
+    strcat(buffer, marks);
+    strcat(buffer, "\n");
 }
-int main() { 
-int choice; 
-Student s; 
-char key[15]; 
-while(1) { 
-cout << "1.Insert\n" 
-<< "2.Display\n" 
-<< "3.Search\n" 
-<< "4.Modify\n" 
-<< "5.Exit\n" << endl; 
-cin >> choice; 
-switch(choice) { 
-case 1: 
-s.insert(); 
-cout << "Done!" << endl; 
-break; 
-case 2: 
-cout << "The contents are: " << endl; 
-s.display(); 
-cout << "Done!" << endl; 
-break; 
-case 3: 
-cout << "Enter the key USN: "; 
-cin >> key; 
-s.search(key); 
-cout << "Done!" << endl; 
-break; 
-case 4: 
-cout << "Enter the USN to modify: "; 
-cin >> key; 
-s.modify(key); 
-cout << "Done!" << endl; 
-break; 
-default: 
-return 0; 
-} 
-} 
+
+void Student::unpack()
+{
+    char *p;
+    p = strtok(buffer, "|");
+    strcpy(usn, p);
+    p = strtok(NULL, "|");
+    strcpy(name, p);
+    p = strtok(NULL, "|");
+    strcpy(sem, p);
+    p = strtok(NULL, "\n");
+    strcpy(marks, p);
+}
+
+void Student::insert()
+{
+    getData();
+    pack(); // packs the data into buffer
+    ofstream fout("record.txt", ios::app);
+    fout << buffer;
+    fout.close();
+}
+
+void Student::display()
+{
+    ifstream fin("record.txt");
+    while (!fin.eof())
+    {
+        fin.getline(buffer, SIZE + 1, '\n');
+        if (fin.fail())
+            break;
+        unpack();
+        putData();
+    }
+    fin.close();
+}
+
+void Student::search(char *key)
+{
+    ifstream fin("record.txt");
+    int count = 0;
+    while (!fin.eof())
+    {
+        fin.getline(buffer, SIZE + 1, '\n');
+        if (fin.fail())
+            break;
+        unpack();
+        if (strcmp(usn, key) == 0)
+        {
+            putData();
+            count++;
+        }
+    }
+    cout << "Total records found: " << count << endl;
+    fin.close();
+}
+
+void Student::modify(char *key)
+{
+    ifstream fin("record.txt");
+    ofstream fout("temp.txt");
+    int count = 0;
+    while (!fin.eof())
+    {
+        fin.getline(buffer, SIZE + 1, '\n');
+        if (fin.fail())
+            break;
+        unpack();
+        if (strcmp(usn, key) == 0)
+        {
+            getData();
+            count++;
+        }
+        pack();
+        fout << buffer;
+    }
+    if (count == 0)
+        cout << "USN not found." << endl;
+    else
+        cout << "Modified. " << endl;
+
+    fin.close();
+    fout.close();
+    remove("record.txt");
+    rename("temp.txt", "record.txt");
+}
+int main()
+{
+    int choice;
+    Student s;
+    char key[15];
+    while (1)
+    {
+        cout << "1.Insert\n"
+             << "2.Display\n"
+             << "3.Search\n"
+             << "4.Modify\n"
+             << "5.Exit\n"
+             << endl;
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            s.insert();
+            cout << "Done!" << endl;
+            break;
+        case 2:
+            cout << "The contents are: " << endl;
+            s.display();
+            cout << "Done!" << endl;
+            break;
+        case 3:
+            cout << "Enter the key USN: ";
+            cin >> key;
+            s.search(key);
+            cout << "Done!" << endl;
+            break;
+        case 4:
+            cout << "Enter the USN to modify: ";
+            cin >> key;
+            s.modify(key);
+            cout << "Done!" << endl;
+            break;
+        default:
+            return 0;
+        }
+    }
 }
--- Program4.cpp
+++ Program4.cpp
@@ -1,146 +1,148 @@
-#include <iostream> 
-#include <fstream> 
-#include <string> 
-#include <sstream> 
-using namespace std; 
-class student 
-{ 
-public: 
-string USN; 
-string Name; 
-string Branch; 
-int Semester; 
-string buffer; 
-int count; 
- 
-int rrn_list[100]; 
- 
-void read_data(); 
-void pack(); 
-void write_to_file(); 
-void unpack(); 
-void create_rrn(); 
-void search_by_rrn(int); 
-}; 
-void student::read_data() 
-{ 
-cout<<"\nUsn:"; 
-cin>>USN; 
-cout<<"\nName:"; 
-cin>>Name; 
-cout<<"\nBranch:"; 
-cin>>Branch; 
-cout<<"\nsemester:"; 
-cin>>Semester; 
-} 
- 
- 
-void student::pack() 
-{ 
-string sem; 
-stringstream out; 
-out << Semester; 
-sem = out.str(); 
-buffer.erase(); 
-buffer=USN+'|'+Name+'|'+Branch+'|'+sem+'$'+'\n'; 
-} 
- 
- 
-void student::write_to_file() 
-{ 
-int pos; 
-fstream file; 
-file.open("1.txt",ios::out|ios::app); 
-pos=file.tellp(); 
-file<<buffer; 
-file.close(); 
-rrn_list[++count]=pos; 
-} 
- 
- 
-void student::unpack() 
-{ 
-string sem; 
-int ch=1,i=0; 
-USN.erase(); 
-while (buffer[i]!='|') 
-USN+=buffer[i++]; 
- 
-Name.erase(); 
-i++; 
-while (buffer[i]!='|') 
-Name+=buffer[i++]; 
- 
-Branch.erase(); 
-i++; 
-while (buffer[i]!='|') 
-Branch+=buffer[i++]; 
- 
-sem.erase(); 
-i++; 
-while (buffer[i]!='$') 
-sem+=buffer[i++]; 
-istringstream out(sem); 
-out>>Semester; 
- 
-} 
- 
-void student::create_rrn() 
-{ 
-ifstream file; 
-int pos; 
-count=-1; 
-file.open("1.txt",ios::in); 
-while (!file.eof()) 
-{ 
-pos=file.tellg(); 
-buffer.erase(); 
-getline(file,buffer); 
-rrn_list[++count]=pos; 
-} 
-file.close(); 
-} 
- 
- 
-void student::search_by_rrn(int rrn) 
-{ 
-int pos=-1; 
-fstream file; 
-if (rrn>count) cout<<"\n Not Found"; 
-else{ 
-buffer.erase(); 
-file.open("1.txt"); 
-pos=rrn_list[rrn]; 
-file.seekp(pos,ios::beg); 
-getline(file,buffer); 
-cout<<"\n"<<buffer<<"\n" ; 
-} 
-} 
- 
- 
-int main() 
-{ 
-int choice,rrn; 
-student s1; 
- 
-s1.create_rrn(); 
-while(1){ 
-cout <<"\nMain Menu\n 1.Add \n\n 2.Search 3.EXIT\n\nEnter the choice:"; 
-cin>>choice; 
-switch (choice) 
-{ 
-case 1: cout<<"Data\n"; 
-s1.read_data(); 
-s1.pack(); 
-s1.write_to_file(); 
-break; 
-case 2: cout <<"\n\nEnter the RRN"; 
-cin>>rrn; 
-s1.search_by_rrn(rrn); 
-break; 
-case 3: return 0; 
-default: cout<<"\n\nWrong Choice"; 
-break; 
-} 
-} 
-} 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
+class student
+{
+public:
+    string USN;
+    string Name;
+    string Branch;
+    int Semester;
+    string buffer;
+    int count;
+
+    int rrn_list[100];
+
+    void read_data();
+    void pack();
+    void write_to_file();
+    void unpack();
+    void create_rrn();
+    void search_by_rrn(int);
+};
+void student::read_data()
+{
+    cout << "\nUsn:";
+    cin >> USN;
+    cout << "\nName:";
+    cin >> Name;
+    cout << "\nBranch:";
+    cin >> Branch;
+    cout << "\nsemester:";
+    cin >> Semester;
+}
+
+void student::pack()
+{
+    string sem;
+    stringstream out;
+    out << Semester;
+    sem = out.str();
+    buffer.erase();
+    buffer = USN + '|' + Name + '|' + Branch + '|' + sem + '$' + '\n';
+}
+
+void student::write_to_file()
+{
+    int pos;
+    fstream file;
+    file.open("1.txt", ios::out | ios::app);
+    pos = file.tellp();
+    file << buffer;
+    file.close();
+    rrn_list[++count] = pos;
+}
+
+void student::unpack()
+{
+    string sem;
+    int ch = 1, i = 0;
+    USN.erase();
+    while (buffer[i] != '|')
+        USN += buffer[i++];
+
+    Name.erase();
+    i++;
+    while (buffer[i] != '|')
+        Name += buffer[i++];
+
+    Branch.erase();
+    i++;
+    while (buffer[i] != '|')
+        Branch += buffer[i++];
+
+    sem.erase();
+    i++;
+    while (buffer[i] != '$')
+        sem += buffer[i++];
+    istringstream out(sem);
+    out >> Semester;
+}
+
+void student::create_rrn()
+{
+    ifstream file;
+    int pos;
+    count = -1;
+    file.open("1.txt", ios::in);
+    while (!file.eof())
+    {
+        pos = file.tellg();
+        buffer.erase();
+        getline(file, buffer);
+        rrn_list[++count] = pos;
+    }
+    file.close();
+}
+
+void student::search_by_rrn(int rrn)
+{
+    int pos = -1;
+    fstream file;
+    if (rrn > count)
+        cout << "\n Not Found";
+    else
+    {
+        buffer.erase();
+        file.open("1.txt");
+        pos = rrn_list[rrn];
+        file.seekp(pos, ios::beg);
+        getline(file, buffer);
+        cout << "\n"
+             << buffer << "\n";
+    }
+}
+
+int main()
+{
+    int choice, rrn;
+    student s1;
+
+    s1.create_rrn();
+    while (1)
+    {
+        cout << "\nMain Menu\n 1.Add \n\n 2.Search 3.EXIT\n\nEnter the choice:";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            cout << "Data\n";
+            s1.read_data();
+            s1.pack();
+            s1.write_to_file();
+            break;
+        case 2:
+            cout << "\n\nEnter the RRN";
+            cin >> rrn;
+            s1.search_by_rrn(rrn);
+            break;
+        case 3:
+            return 0;
+        default:
+            cout << "\n\nWrong Choice";
+            break;
+        }
+    }
+}
